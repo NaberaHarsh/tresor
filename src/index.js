a94c8316fb97d1358f7be89ba238e5447f543cc8
SubhashKumhar <aks6382@gmail.com>
2019-10-16 23:56:11 +0530

    PWA check

--- src/index.js
+++ src/index.js
@@ -15,27 +15,33 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-// window.addEventListener('load',() =>{
-// //  fetch();
-//   registerServiceWorker();
-// })
 
 
-// async function registerServiceWorker(){
-//   if ('serviceWorker' in navigator) {
-//     try{
-//       await navigator.serviceWorker.register('./serviceWorker.js');
-//       console.log('seviceWorker registertion successful')
-//     }
-//       catch (err) {
-//         console.log('serviceWorker registertion failed');
-//       }
-//     }
-//   }
+
+
+
+
+window.addEventListener('load',() =>{
+//  fetch();
+  registerServiceWorker();
+})
+
+
+async function registerServiceWorker(){
+  if ('serviceWorker' in navigator) {
+    try{
+      await navigator.serviceWorker.register('./serviceWorker.js');
+      console.log('seviceWorker registertion successful')
+    }
+      catch (err) {
+        console.log('serviceWorker registertion failed');
+      }
+    }
+  }
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register('sdasd');
 
 
