cdf137a685c15318e9e6e7dfb2ff4778568f0344
Harsh Nabera <harshnabera30@gmail.com>
2020-01-03 17:28:09 +0530

    json data added to cart

--- src/Router/router.js
+++ src/Router/router.js
@@ -21,10 +21,46 @@ class Routes extends Component {
       refreshHead : true
     }
 
+    this.state.db={
+      Cart:[
+        {
+          name:'Pearl Earring',
+          image:'.././favicon/rrr.jpg',
+          desc:'This is a sample description about the earring. It has size, color and merial used.',
+          price:'999',
+          quantity:'1',
+        },
+        {
+          name:'New pendant',
+          image:'.././favicon/pendant.jpg',
+          desc:'This is a sample description about the Pendant. It has size, color and merial used.',
+          price:'999',
+          quantity:'1'
+        
+        },
+        {
+          name:'Bracelet',
+          image:'.././favicon/bracelet.jpg',
+          desc:'This is a sample description about the Bracelet. It has size, color and merial used.',
+          price:'999',
+          quantity:'1'
+        }
+      ]
+    }
+
 
     
   }
 
+  changeQuantity(p,e){
+    let db = this.state.db;
+    let i = db.Cart.indexOf(p)
+     db.Cart[i].quantity = parseInt(e.target.value); 
+    this.setState(
+      {db:db}
+    )
+    console.log(db);
+  }
 
   render() {
     
@@ -53,7 +89,7 @@ class Routes extends Component {
           />} />
           <PrivateRoute path='/details/:id' component={Details} />
            <Route path="/register" exact component={Register} />
-                      <Route path="/cart" exact component={Cart} />
+                      <Route path="/cart" exact render={()=> <Cart db={this.state.db} changeQuantity={this.changeQuantity.bind(this)}  /> } />
 
           <Route 
           path="/login" 
--- src/component/Cart.js
+++ src/component/Cart.js
@@ -7,6 +7,8 @@ import { Grid, Card, Table } from '@material-ui/core';
 import { TableHeader } from 'semantic-ui-react';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';import { withStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
 
 const styles = theme => ({
   '@global': {
@@ -14,20 +16,17 @@ const styles = theme => ({
       backgroundColor: theme.palette.common.white,
     },
   },
-  paper: {
-
-    marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(2),
-    display: 'flex',
-    flexDirection: 'row', 
-
-  }
-  ,
-  check: {
-    marginTop: theme.spacing(2),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: "center"
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex"
+    }
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
   }
 });
 
@@ -39,6 +38,15 @@ class Cart extends Component{
   }
 
 
+  change(action){
+    let quantity=0;
+    if(action=="plus")
+    quantity++;
+    if(action=="minus")
+    quantity--;
+    return quantity;
+  }
+
   render(){
       const { classes } = this.props;
 
@@ -48,37 +56,46 @@ class Cart extends Component{
 <hr />
   <p>Shopping Cart</p>
   <Grid container space={3}>
-           
+  {this.props.db.Cart.map(p=>
+
 <Grid md={8} lg={8} sm={12} xs={12} container space={3} >
   <hr />
 
-
 <Grid md={6} lg={6} sm={6} xs={6} >
 
-<center><img class="img-fluid" src='https://cdn.shopify.com/s/files/1/0789/0141/products/D7302TZ_1024x1024.png?v=1541094254' style={{maxHeight:280, maxWidth:280, height:'auto', width:'100%', border:"solid #515151 1px"}}  /></center>
+<center><img class="img-fluid" src={p.image} style={{maxHeight:280, maxWidth:280, height:'auto', width:'100%', border:"solid #515151 1px"}}  /></center>
 
 </Grid>
 <Grid  md={6} lg={6} sm={6} xs={6} container space={2} >
   <Grid md={6} lg={6} sm={12} xs={12} container space={2} style={{paddingLeft:'10px'}}>
       
-<h6 style={{ textAlign:'center', color:'black'}}>Product Name</h6>
-<p>Sample Description about the product</p><br />
+<div style={{fontWeight:'bold', textAlign:'center', color:'black', display:'inline' }}>{p.name} </div>
+  <div style={{display:'inline'}}>{p.desc}</div>
 <Grid md={6} lg={6} sm={8} xs={8}>
-<div class="input-group plus-minus-input" style={{display:'inline'}}>
+{/* <div class="input-group plus-minus-input" style={{display:'inline'}}>
 <form>
 <div class="input-group-button" style={{display:'inline'}}>
-  <button type="button" class="button hollow circle" data-quantity="minus" data-field="quantity" style={{display:'inline'}}>
+  <button type="button" class="button hollow circle" data-action="minus" data-field="quantity" style={{display:'inline'}} onClick={this.change(data-action)}>
+    
     <i class="fa fa-minus" aria-hidden="true"></i>
   </button>
 </div>
-<input class="input-group-field" type="text" name="quantity" value="0" size="1" style={{display:'inline', textAlign:'center'}} />
+<input class="input-group-field" type="text" name="quantity" value="0" size="1" style={{display:'inline', textAlign:'center'}} onChange={(e)=>{this.props.changeQuantity(p,e)}}  >{this.props.change}</input>
 <div class="input-group-button" style={{display:'inline'}}>
-  <button type="button" class="button hollow circle" data-quantity="plus" data-field="quantity" style={{display:'inline'}}>
-    <i class="fa fa-plus" aria-hidden="true"></i>
-  </button>
+ <button type="button" class="button hollow circle" data-action="plus" data-field="quantity" style={{display:'inline'}} onClick={this.change(data-action)}>
+ <i class="fa fa-plus" aria-hidden="true"></i>
+ </button> 
 </div>
 </form>
-</div>
+</div> */}
+<form>
+                <select onChange={(e)=>{this.props.changeQuantity(p,e)}}>
+                  
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option></select></form>
 
 
                   </Grid>
@@ -87,54 +104,15 @@ class Cart extends Component{
 </Grid>
 </Grid>
 <Grid md={6} lg={6} sm={12} xs={12}>
-<h6 style={{ textAlign:'center', color:'black'}}>999/- Rs.</h6>
+  <h6 style={{ textAlign:'center', color:'black'}}>{p.price}/- Rs. </h6>
   </Grid>
 </Grid>
 
 <hr />
 
 
-<Grid md={6} lg={6} sm={6} xs={6} >
 
-<center><img class="img-fluid" src='https://cdn.shopify.com/s/files/1/0789/0141/products/D7302TZ_1024x1024.png?v=1541094254' style={{maxHeight:280, maxWidth:280, height:'auto', width:'100%', border:"solid #515151 1px"}}  /></center>
-
-</Grid>
-<Grid  md={6} lg={6} sm={6} xs={6} container space={2} >
-  <Grid md={6} lg={6} sm={12} xs={12} container space={2} style={{paddingLeft:'10px'}}>
-      
-<h6 style={{ textAlign:'center', color:'black'}}>Product Name</h6>
-<p>Sample Description about the product</p><br />
-<Grid md={6} lg={6} sm={8} xs={8}>
-<div class="input-group plus-minus-input" style={{display:'inline'}}>
-<form>
-<div class="input-group-button" style={{display:'inline'}}>
-  <button type="button" class="button hollow circle" data-quantity="minus" data-field="quantity" style={{display:'inline'}}>
-    <i class="fa fa-minus" aria-hidden="true"></i>
-  </button>
-</div>
-<input class="input-group-field" type="text" name="quantity" value="0" size="1" style={{display:'inline', textAlign:'center'}} />
-<div class="input-group-button" style={{display:'inline'}}>
-  <button type="button" class="button hollow circle" data-quantity="plus" data-field="quantity" style={{display:'inline'}}>
-    <i class="fa fa-plus" aria-hidden="true"></i>
-  </button>
-</div>
-</form>
-</div>
-
-
-                  </Grid>
-                  <Grid md={6} lg={6} sm={4} xs={4}>
-                  <DeleteIcon />
-</Grid>
-</Grid>
-<Grid md={6} lg={6} sm={12} xs={12}>
-<h6 style={{ textAlign:'center', color:'black'}}>999/- Rs.</h6>
-  </Grid>
-</Grid>
-
-<hr />
-
-</Grid>
+</Grid>)}
 
 <Grid md={4} lg={4} sm={12} xs={12} >
   
@@ -142,7 +120,7 @@ class Cart extends Component{
 <div style={{border:'solid goldenrod 1px', textAlign:'center'}}>
             
   <h2 style={{ paddingTop:'15px', textAlign:'center'}}>Checkout</h2>
-  <h6 style={{color:'black'}}>SubTotal: 999</h6>
+  <h6 style={{color:'black'}}>SubTotal: {this.props.db.Cart.reduce((sum,p)=>sum+p.price*p.quantity,0)}</h6>
   <br />
   <Button variant="contained" style={{backgroundColor:'black', color:'white'}}>Proceed To Buy</Button> 
    <br /><br />
